21846cb62834c905d188900dd39972e0c235ac06
venamartin <66525548+venamartin@users.noreply.github.com>
2023-06-08 15:19:14 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ then
     echo "BT name is set"
 else
     echo "BT name is being generated"
-    sudo mount -o remount,rw /boot
+    sudo raspi-config nonint do_overlayfs 0
     echo -n 'PRETTY_HOSTNAME=pitnc ' > machine-info
     hcitool dev | grep -o "[[:xdigit:]:]\{11,17\}" >> machine-info
     sudo mv ./machine-info /etc/machine-info
+    sudo raspi-config nonint do_overlayfs 1
 fi
